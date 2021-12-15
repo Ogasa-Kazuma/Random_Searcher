3c68b05e83dba8c706a9c4a5403ea92af92c1b06
Ogasa Kazuma <knct0831@gmail.com>
2021-12-15 19:44:45 +0900

    add / test

--- Linear_Function.py
+++ Linear_Function.py
@@ -1,29 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
 class LinearFunction:
 
     def __init__(self, maxValue, maxPoint, zeroPoint):
+
+        self.__maxValue = maxValue
         self.__zeroPoint = zeroPoint
         self.__maxPoint = maxPoint
 
-    def Calc(self, arg):
+    def Calc(self, x):
+        "xとyの線形関数の値を計算"
 
-        dec = self.__CalcDec(arg)
-        #1、じゃなくてmax_valueのほうがいいかも
-        probabilit = maxValue - dec
+        #入力に応じた出力の減少を計算
+        dec = self.__CalcDec(x)
+        y = self.__maxValue - dec
 
-        if(probability <= 0):
-            probability = 0
+        if(y <= 0):
+            y = 0
 
-            return probability
+        return y
 
 
-    def __CalcDec(self, arg):
-        dec_ratio = 1 / (self.__ZeroPoint() / 1)
-        diff = self.__MaxPoint() - arg
+    def __CalcDec(self, x):
+        "入力に応じた出力の減少を計算"
+        #座標と値の減少比
+        dec_ratio = self.__DecRatio()
+        #最高値を示す座標から離れるほど減少幅が大きくなる
+        diff = self.__MaxPoint() - x
         dec =  dec_ratio * diff
         dec = abs(dec)
 
         return dec
+
+
+    def __DecRatio(self):
+        #線形変化
+        r = 1 / (self.__ZeroPoint() / self.__MaxValue())
+        return r
 
 
     def __MaxPoint(self, value = None):
@@ -32,8 +46,31 @@ class LinearFunction:
 
         self.__maxPoint = value
 
+
     def __ZeroPoint(self, value = None):
         if(value is None):
             return self.__zeroPoint
 
         self.__zeroPoint = value
+
+
+    def __MaxValue(self, value = None):
+        if(value is None):
+            return self.__maxValue
+
+        self.__maxValue = value
+
+
+
+def main():
+
+    calculator = LinearFunction(1, 0, 120)
+    y = list()
+    degrees = np.arange(-180.0, 180.0, 0.1)
+    for i in degrees:
+        y.append(calculator.Calc(i))
+
+    plt.scatter(degrees, y, c = 'red')
+
+if __name__ == "__main__":
+    main()
