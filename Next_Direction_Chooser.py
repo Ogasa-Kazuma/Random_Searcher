d31b02e00bad8781da6a50b83102ea865dd9a019
Ogasa Kazuma <knct0831@gmail.com>
2021-12-15 22:32:05 +0900

    add: func Choice and func Clear
    body: to choice item

--- Next_Direction_Chooser.py
+++ Next_Direction_Chooser.py
@@ -1,8 +1,71 @@
 
+import random
 
 class NextDirectionChooser:
 
     def __init__(self, degreeDistMaker):
-        self.__degeeDistMaker = degreeDistMaker
+        self.__degreeDistMaker = degreeDistMaker
+        self.__degreeDist = None
+        self.__isInitialized = False
 
-    def Choose(self, degreeRange):
+
+    def Choice(self, degreeRange, delDegreeRange):
+        if(not self.__Initialized()):
+            dist = degreeDistMaker.Make(degreeRange)
+            self.__Dist(dist)
+
+
+        degree = random.choice(self.__Dist())
+        self.__ExcludeSearchedDegree(degree, delDegreeRange)
+
+        return degree
+
+
+    def Clear(self):
+        self.__isInitialized = False
+        self.__Dist(None)
+
+
+    def __ExcludeSearchedDegree(self, deg, degRange):
+
+        #0より小さい値は除去
+        lwr = self.__DelMinusArea(deg - degRange)
+        upr = self.__DelMinusArea(deg + degRange)
+
+        #探索した角度とその周辺の角度は次の探索で使用しない
+        narrowed_dist = self.__Exclude(lwr, upr)
+
+        #更新
+        self.__Dist(narrowed_dist)
+
+        return None
+
+
+    def __Exclude(self, lwr, upr):
+        "角度確率分布リストから指定範囲の角度を取り除く"
+        narrowed_dist = list()
+        now_dist = self.__Dist()
+        for deg_i in now_dist:
+            needExclude = (deg_i >= lwr and deg_i <= upr)
+            if(needExclude):
+                continue
+
+            narrowed_dist.append(deg_i)
+
+        return narrowed_dist
+
+
+    def __Dist(self, value = None):
+        if(value is None):
+            return self.__degreeDist
+        self.__degreeDist = value
+
+
+    def __DelMinusArea(self, number):
+        if(number <= 0):
+            number = 0
+
+        return number
+
+    def __UpdateDist(self, new_dist):
+        self.__Dist(new_dist)
