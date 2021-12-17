7b646bd1f465932ae6631be552ba208b1fe7dec4
Ogasa Kazuma <knct0831@gmail.com>
2021-12-17 19:54:17 +0900

    refactor: params of linear func object, To class member

--- test/Linear_Function_Ret_Int.py
+++ test/Linear_Function_Ret_Int.py
@@ -12,14 +12,35 @@ importlib.reload(Linear_Function)
 importlib.reload(Linear_Function_Ret_Int)
 
 
+class TestParams:
+
+    def __init__(self, maxValue, maxPoint, zeroPoint):
+        self.__maxValue = maxValue
+        self.__maxPoint = maxPoint
+        self.__zeroPoint = zeroPoint
+
+    def GetMaxValue(self):
+        return self.__maxValue
+
+    def GetMaxPoint(self):
+        return self.__maxPoint
+
+    def GetZeroPoint(self):
+        return self.__zeroPoint
+
+
 def main():
 
-    linearFunc = Linear_Function.LinearFunction(maxValue = 1, maxPoint = 0, zeroPoint = 30)
+    params = TestParams(1, 0, 30)
+
+    linearFunc = \
+    Linear_Function.LinearFunction(params.GetMaxValue(), params.GetMaxPoint(), params.GetZeroPoint())
+
+
     intFunc = Linear_Function_Ret_Int.LinearFunctionRetInt(linearFunc)
-    result = intFunc.Calc(-14)
+
+    result = intFunc.Calc(14.9999999999999999999)
     print(result)
-
-
 
 if __name__ == "__main__":
     main()
