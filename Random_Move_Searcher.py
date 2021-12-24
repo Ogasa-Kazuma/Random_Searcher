64afadaded605f801460a8a26e196d9c36c7ade6
Ogasa Kazuma <knct0831@gmail.com>
2021-12-24 20:54:33 +0900

    pref: remove time limitter

--- Random_Move_Searcher.py
+++ Random_Move_Searcher.py
@@ -4,34 +4,35 @@ import random
 
 class RandomMoveSearcher:
 
-    def __init__(self, timeLimitter, dataRecorder, lineSearcher):
+    def __init__(self, dataRecorder, lineSearcher):
 
-        self.__timeLimitter = timeLimitter
         self.__dataRecorder = dataRecorder
         self.__lineSearcher = lineSearcher
 
 
-    def Search(self, start_x, start_y, max_x, max_y, min_x, min_y, speed, threshold):
+    def Search(self, start_x, start_y, start_time, max_time, max_x, max_y, min_x, min_y, speed, threshold):
 
         x = start_x
         y = start_y
+        t = start_time
 
         while(1):
             next_x, next_y = self.__NextPosition(min_x, min_y, max_x, max_y)
-            x, y, t, pollution = self.__SearchStraightLine(threshold, x, next_x, y, next_y, speed)
-            if(self.__IsTimeOver()):
+            x, y, t, pollution = self.__SearchStraightLine(threshold, x, next_x, y, next_y, start_time, max_time, speed)
+            if(self.__IsTimeOver(t, max_time)):
                 return x, y, t, pollution
 
             if(self.__IsFound(threshold, pollution)):
                 return x, y, t, pollution
 
-    def __SearchStraightLine(self, threshold, x, next_x, y, next_y, speed):
-        kwargs = dict(threshold = threshold, x1 = x, x2 = next_x, y1 = y, y2 = next_y, speed = speed)
+    def __SearchStraightLine(self, threshold, x, next_x, y, next_y, start_time, max_time, speed):
+        kwargs = dict(threshold = threshold, x1 = x, x2 = next_x, y1 = y, y2 = next_y, start_time = start_time, max_time = max_time, speed = speed)
         x, y, t, pollution = self.__lineSearcher.Search(kwargs)
         return x, y, t, pollution
 
-    def __IsTimeOver(self):
-        return self.__timeLimitter.IsTimeOver()
+    def __IsTimeOver(self, time, max_time):
+        isTimeOver = (time >= max_time)
+        return isTimeOver
 
 
 
@@ -50,6 +51,8 @@ class RandomMoveSearcher:
         xList, yList, _, _ = self.__dataRecorder.GetAllData()
         if(x in xList and y in yList):
             return True
+
+        return False
 
 
     def __IsFound(self, threshold, pollution):
--- test/Random_Move_Searcher.py
+++ test/Random_Move_Searcher.py
@@ -10,6 +10,7 @@ sys.path.append(os.pardir)
 import importlib
 
 import Searching_Data_Recorder
+import Data_Recorder
 import Pollution_Reader
 import Pickle_Reader
 import Field
@@ -19,6 +20,7 @@ import Time_Limitter
 import Random_Move_Searcher
 
 importlib.reload(Searching_Data_Recorder)
+importlib.reload(Data_Recorder)
 importlib.reload(Pollution_Reader)
 importlib.reload(Pickle_Reader)
 importlib.reload(Field)
@@ -31,16 +33,24 @@ def main():
 
     pklReader = Pickle_Reader.PickleReader()
     pollutionReader = Pollution_Reader.PollutionReader("/home/kazuma/研究/RandomSearcher/DataLog/2021年/12月/3日/18時/40分/6秒/", "pkl", pklReader)
-    dataRecorder = Searching_Data_Recorder.SearchingDataRecorder(pollutionReader)
+    dataRecorder = Data_Recorder.DataRecorder()
     field = Field.FieldClass(dict(uprX = 99, uprY = 99, uprT = 2999, lwrX = 0, lwrY = 0, lwrT = 0))
-    onePointSearcher = One_Point_Searcher.OnePointSearcher(dataRecorder, field)
-    timeLimitter = Time_Limitter.TimeLimitter(limitFromStart_s = 200)
-    timeLimitter.Start(0)
+    onePointSearcher = One_Point_Searcher.OnePointSearcher(dataRecorder, field, pollutionReader)
 
-    lineSearcher = Straight_Line_Searcher.StraightLineSearcher(onePointSearcher, timeLimitter)
-    randomMoveSearcher = Random_Move_Searcher.RandomMoveSearcher(timeLimitter, dataRecorder, lineSearcher)
-    print(randomMoveSearcher.Search(start_x = 50, start_y = 50, max_x = 99, max_y = 99, min_x = 0, min_y = 0, speed = 2, threshold = 30))
+    lineSearcher = Straight_Line_Searcher.StraightLineSearcher(onePointSearcher)
+    randomMoveSearcher = Random_Move_Searcher.RandomMoveSearcher(dataRecorder, lineSearcher)
+    print(randomMoveSearcher.Search(start_x = 50,\
+                                    start_y = 50,\
+                                    start_time = 10,\
+                                    max_time = 15,\
+                                    max_x = 99,\
+                                    max_y = 99,\
+                                    min_x = 0,\
+                                    min_y = 0,\
+                                    speed = 2,\
+                                    threshold = 50))
 
+    print(pollutionReader.Read(56, 63, 30.5))
 
 if __name__ == "__main__":
     main()
